7e69fbf364b4cb3fe5654229e4900dfb86ac2c5a
Lesmiscore <nao20010128@gmail.com>
2022-03-26 16:59:51 +0900

    generate subject data

--- src/generate_pages.mjs
+++ src/generate_pages.mjs
@@ -2,6 +2,8 @@
 
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import fs from "fs";
+import util from "util";
 
 // open the database
 const db = await open({
@@ -66,16 +68,47 @@ async function countRows(table) {
 async function* enumerateRows(table, pageSize = 30) {
   const total = await countRows(table);
   for (let offset = 0; offset < total; offset += pageSize) {
-    const { rowsCount, rows } = await betterEach(`SELECT * FROM ${table} LIMIT ${pageSize} OFFSET ${offset};`);
-    console.log(rowsCount);
+    const { rows } = await betterEach(`SELECT * FROM ${table} LIMIT ${pageSize} OFFSET ${offset};`);
     yield* rows;
   }
 }
 
-// WIP
+function createDir(dir) {
+  return util.promisify(fs.mkdir)(dir, { recursive: true, });
+}
+
+async function inlineVars(obj, keys) {
+  for (const k of keys) {
+    obj[k] = await db.get(`SELECT * FROM ${k}_table WHERE id = ?;`, [obj[`${k}_id`]]);
+    delete obj[`${k}_id`];
+  }
+  return obj;
+}
+
+async function inlineMonolingual(obj, lang) {
+  for (let k of Object.keys(obj)) {
+    if (!k.endsWith("_id")) {
+      continue;
+    }
+    if (typeof obj[k] !== "number") {
+      continue;
+    }
+    k = k.substring(0, k.length - 3);
+    console.log("proc", k);
+    obj[k] = (await db.get(`SELECT ${lang} FROM ${k}_table WHERE id = ?;`, [obj[`${k}_id`]]))[lang];
+    delete obj[`${k}_id`];
+  }
+  return obj;
+}
+
+await createDir("generated/");
 
 console.log(await countRows("subjects"));
 for await (const row of enumerateRows("subjects")) {
   // inline bilingual entries
   // _references -> references
+  await inlineVars(row, ["name", "instructor", "present_lang", "grades"]);
+  await inlineMonolingual(row, "ja");
+  console.log(row);
+  break;
 }
